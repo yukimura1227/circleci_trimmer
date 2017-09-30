90eeeb384b9e4e32fba50afdd151ad8eabf5db18
yukimura1227 <takamura1227@gmail.com>
2017-09-30 17:28:45 +0000

    add token command and token_show command

--- spec/circleci_trimmer/command_spec.rb
+++ spec/circleci_trimmer/command_spec.rb
@@ -10,4 +10,39 @@ RSpec.describe CircleciTrimmer::Command do
     output = capture(:stdout) { described_class.start(%w(example)) }
     expect(output.chomp).to eq "I'm a thor task!"
   end
+  describe 'show token' do
+    context 'no token file' do
+      before do
+        File.delete(CircleciTrimmer::Command.token_path)
+      end
+      it 'show warning message cause yet token registered' do
+        output = capture(:stdout) { described_class.start(%w(show_token)) }
+        expect(output.chomp).to eq \
+          'there is no registered token. please register token'
+      end
+    end
+    context 'exists token file' do
+      before do
+        File.open(CircleciTrimmer::Command.token_path, 'w') do |f|
+          f.write('dummy')
+        end
+      end
+      it 'show registered token value' do
+        output = capture(:stdout) { described_class.start(%w(show_token)) }
+        expect(output.chomp).to eq \
+          'dummy'
+      end
+    end
+  end
+  describe 'token [api_token_value]' do
+    before do
+      File.delete(CircleciTrimmer::Command.token_path)
+    end
+    it 'store api_token_value' do
+      described_class.start(%w(token dummy_token_value))
+      stored_token =
+        File.open(CircleciTrimmer::Command.token_path, 'r', &:read)
+      expect(stored_token.chomp).to eq 'dummy_token_value'
+    end
+  end
 end
