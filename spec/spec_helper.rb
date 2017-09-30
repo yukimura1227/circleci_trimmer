c974c23be08321e141f41379697683866171c719
yukimura1227 <takamura1227@gmail.com>
2017-09-30 17:27:42 +0000

    add original environment variable for know test mode execute

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ require 'bundler/setup'
 require 'circleci_trimmer'
 
 RSpec.configure do |config|
+  ENV['TEST_FLAG'] = 'true'
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = '.rspec_status'
 
