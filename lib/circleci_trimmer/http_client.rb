2c9145585e511bc1c75266c3675b671a54b94796
yukimura1227 <takamura1227@gmail.com>
2017-10-01 13:26:50 +0000

    add cached for projects api response

--- lib/circleci_trimmer/http_client.rb
+++ lib/circleci_trimmer/http_client.rb
@@ -6,13 +6,14 @@ module CircleciTrimmer
   # define api executer
   class HttpClient
     def call_projects
+      return @projects_cache if @projects_cache
       uri = 'https://circleci.com/api/v1.1/projects'
       params = {
         'circle-token' => CircleciTrimmer::Setting.token
       }
       client = HTTPClient.new
       response = client.get(uri, params)
-      response.body
+      @projects_cache = response.body
     end
   end
 end
