6ef67e4cda4e80a5ae4e963c1219fbad7dd5e131
yukimura1227 <takamura1227@gmail.com>
2017-10-02 14:59:01 +0000

    skelton code for calling https://circleci.com/api/v1.1/project/:vcs-type/:username/:project/tree/:branch

--- lib/circleci_trimmer/http_client.rb
+++ lib/circleci_trimmer/http_client.rb
@@ -12,6 +12,9 @@ module CircleciTrimmer
     DOMAIN   = 'circleci.com'.freeze
     COMMON_ENDPOINT = "#{PROTOCOL}//#{DOMAIN}/api/v1.1".freeze
     API_URI_PROJECTS = "#{COMMON_ENDPOINT}/projects".freeze
+    # TODO: 名前付きテンプレート　文字列にしたい
+    API_URI_PROJECT  = "#{COMMON_ENDPOINT}/project/github/%s/%s/tree/%s?circle-token=%s".freeze
+
     def call_projects
       return @projects_cache if @projects_cache
       params = {
@@ -20,6 +23,20 @@ module CircleciTrimmer
       client = HTTPClient.new
       response = client.get(API_URI_PROJECTS, params)
       @projects_cache = response.body
+    end
+
+    def call_user_repo_branch(username, repo_name, branch)
+      uri = format(
+        API_URI_PROJECT,
+        username,
+        repo_name,
+        branch,
+        CircleciTrimmer::Setting.token
+      )
+      # TODO: キャッシュして使いまわした方が良い？HTTPClientを
+      client = HTTPClient.new
+      response = client.get(uri)
+      puts response.body
     end
 
     def list_user_names
